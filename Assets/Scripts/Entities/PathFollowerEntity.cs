af2fc9967e069349996cc388b5252cdb90e2583e
AnthonyMellon_ <anthonymellon@yahoo.com>
2024-11-28 02:14:47 +1300

    Lerping enemy positions for smoother pathing

--- Assets/Scripts/Entities/PathFollowerEntity.cs
+++ Assets/Scripts/Entities/PathFollowerEntity.cs
@@ -5,48 +5,66 @@ using UnityEngine;
 public class PathFollowerEntity : MonoBehaviour
 {
     private Path _currentPath;
-    private int _pathProgress;
+    private int _currentPathPointIndex;
+    private float _progressTowardsTargetPoint;
+    private Vector2Int _targetPoint;
+    private Vector2Int _currentPoint;
 
     private void Start()
     {
-        StartCoroutine(FollowPathRoutine());   
+
     }
 
     private IEnumerator FollowPathRoutine()
     {
         while (true)
         {
-            ProgressOnPath();
+            MoveAlongPath();
             yield return new WaitForSeconds(0.1f);
         }
     }
 
-    private void ProgressOnPath()
+    private void Update()
+    {
+        MoveAlongPath();
+    }
+
+    private void MoveAlongPath()
     {
         // No point trying to progress on a path that doesn't exist 
         if (_currentPath == null) return;
 
-        _pathProgress++;
+        _progressTowardsTargetPoint += Time.deltaTime * 2;
+        if (_progressTowardsTargetPoint >= 1)
+        {
+            GetNextPathPoint();
+        }
 
-        // Have we reached the end of the path?
-        if(_pathProgress == _currentPath.Points.Count)
+        transform.localPosition = Vector2.Lerp(_currentPoint, _targetPoint, _progressTowardsTargetPoint);
+    }
+
+    private void GetNextPathPoint()
+    {
+        _currentPathPointIndex++;
+
+        // If we're out of points we must have reached the target structure
+        if (_currentPathPointIndex == _currentPath.Points.Count - 1)
         {
             _currentPath.TargetStructure.OnReached(this);
             return;
         }
 
-        // Move to the next point along the path
-        Vector3 newPosition = new Vector3(
-            _currentPath.Points[_pathProgress].x,
-            _currentPath.Points[_pathProgress].y
-            );
-        transform.localPosition = newPosition;        
+        _currentPoint = _currentPath.Points[_currentPathPointIndex];
+        _targetPoint = _currentPath.Points[_currentPathPointIndex + 1];
+        _progressTowardsTargetPoint = 0;
     }
 
     public void SetPath(List<Vector2Int> points, StructureBase targetStructure)
     {
         _currentPath = new Path(points, targetStructure);
-        _pathProgress = 0;
+
+        _currentPathPointIndex = -1;
+        GetNextPathPoint();
     }
 
     protected class Path

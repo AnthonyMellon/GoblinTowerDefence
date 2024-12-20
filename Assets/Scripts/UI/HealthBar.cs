8d081fe7c032da7f47e11a9e392650ffb16570c5
AnthonyMellon_ <anthonymellon@yahoo.com>
2024-12-21 02:31:43 +1300

    Healthbars are hidden untill the health is changed and will hide again after a set cooldown

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -1,15 +1,25 @@
 using System;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEngine.GraphicsBuffer;
 
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private AttackableEntity _entity;
+    [SerializeField] private Canvas _canvas;
     [SerializeField] private Image _fill;
     [SerializeField] private Gradient _fillGradient;
-
+    [SerializeField] private float _displayCooldown;
+    private float _currentDisplayCooldown;
     private float _currentFillValue = 1;
+
+    private bool b_shouldDisplay;
+    private bool _shouldDisplay { 
+        get { return b_shouldDisplay; } 
+        set { 
+            b_shouldDisplay = value;
+            UpdateCanvas();
+        } 
+    }
 
     private void OnEnable()
     {
@@ -22,12 +32,41 @@ public class HealthBar : MonoBehaviour
         _entity.OnHealthChange -= OnHealthChanged;
     }
 
+    private void Update()
+    {
+        if(_shouldDisplay)
+        {
+            UpdateCooldown();
+        }
+    }
+
     private void OnHealthChanged(float currHealth, float maxHealth)
     {
         float value = currHealth / maxHealth;
         _currentFillValue = value;
 
+        _shouldDisplay = true;
+        _currentDisplayCooldown = _displayCooldown;
+
         UpdateFill();
+    }
+
+    private void UpdateCooldown()
+    {
+        if(_currentDisplayCooldown > 0)
+        {
+            _currentDisplayCooldown -= Time.deltaTime;
+        }
+
+        if(_currentDisplayCooldown <= 0)
+        {
+            _shouldDisplay = false;
+        }
+    }
+
+    private void UpdateCanvas()
+    {
+        _canvas.gameObject.SetActive(_shouldDisplay);
     }
 
     private void UpdateFill()
